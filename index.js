59b95c14c5629d58fe029de00fb6cd50ed09b69d
Moseti <mosetioba01@gmail.com>
2024-05-31 18:37:33 +0300

    key press fixes

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ document.addEventListener('keydown', (event) => {
         if (button.textContent.toLowerCase() === pressedKey) {
             
             switch (pressedKey) {
+                case 'esc' :
+                    screenElement.textContent += button.textContent;
+                    break;
                 // Function Keys (F1-F12)
                 case pressedKey.startsWith('f'):
                     screenElement.textContent += `F${pressedKey.slice(1)}`;
                     break;
                 // Specific keys with custom classes
                 case 'backspace':
+                    screenElement.textContent += button.textContent;
+                    break;
+
                 case 'tab':
-                case 'caps lock':
+                case 'capslock':
                 case 'enter':
                 case 'shift': 
-                case 'ctrl':  
+                case 'ctrl':
                 case 'alt':   
                     screenElement.textContent += button.textContent;
                     break;
