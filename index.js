8869c86d7a04b9130716a06e34e9a6adf2a3ab4f
Moseti <mosetioba01@gmail.com>
2024-06-02 19:27:35 +0300

    updated mobile

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const screenElement = document.querySelector('.screen p');
 
 document.addEventListener('keydown', (event) => {
-    const pressedKey = event.key;
-
+    const pressedKey = (event.key);
     const utterance = new SpeechSynthesisUtterance(pressedKey);
     speechSynthesis.speak(utterance);
 
     screenElement.textContent += pressedKey;
-    
 })
+
+const inputTrigger = document.querySelector('.input-trigger');
+
+inputTrigger.addEventListener('click', () => {
+    
+    const hiddenInput = document.createElement('input');
+    hiddenInput.type = 'text';
+    hiddenInput.style.position = 'absolute';
+    hiddenInput.style.left = '-9999px';
+
+    document.body.appendChild(hiddenInput);
+    hiddenInput.focus();
+
+
+    
+    setTimeout(() => {
+        document.body.removeChild(hiddenInput);
+    }, 100); 
+});
