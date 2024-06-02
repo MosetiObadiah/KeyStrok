cc7e1dd4eac4ed33f917463aa7f8f0c90dd5755a
Moseti <mosetioba01@gmail.com>
2024-06-02 18:56:49 +0300

    updated UI, added voice

--- index.js
+++ index.js
@@ -1,45 +1,11 @@
-const allButtons = document.querySelectorAll('button');
 const screenElement = document.querySelector('.screen p');
 
 document.addEventListener('keydown', (event) => {
-    const pressedKey = event.key.toLowerCase();
+    const pressedKey = event.key;
 
-    for (const button of allButtons) {
-        if (button.textContent.toLowerCase() === pressedKey) {
-            
-            switch (pressedKey) {
-                case 'esc' :
-                    screenElement.textContent += button.textContent;
-                    break;
-                // Function Keys (F1-F12)
-                case pressedKey.startsWith('f'):
-                    screenElement.textContent += `F${pressedKey.slice(1)}`;
-                    break;
-                // Specific keys with custom classes
-                case 'backspace':
-                    screenElement.textContent += button.textContent;
-                    break;
+    const utterance = new SpeechSynthesisUtterance(pressedKey);
+    speechSynthesis.speak(utterance);
 
-                case 'tab':
-                case 'capslock':
-                case 'enter':
-                case 'shift': 
-                case 'ctrl':
-                case 'alt':   
-                    screenElement.textContent += button.textContent;
-                    break;
-                // Arrow Keys
-                case 'arrowup':
-                case 'arrowdown':
-                case 'arrowleft':
-                case 'arrowright':
-                    screenElement.textContent += button.textContent;
-                    break;
-                // Other Keys
-                default:
-                    screenElement.textContent += pressedKey;
-            }
-            break;
-        }
-    }
-});
+    screenElement.textContent += pressedKey;
+    
+})
